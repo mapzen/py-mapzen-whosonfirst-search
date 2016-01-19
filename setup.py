e974e31e240737c4ef397f2a952223d9c569b7f0
Dan Phiffer <dan@phiffer.org>
2016-01-18 17:34:50 -0800

    🔧 Added VERSION

--- setup.py
+++ setup.py
@@ -3,22 +3,27 @@
 from setuptools import setup, find_packages
 
 packages = find_packages()
+version = open("VERSION").read()
 desc = open("README.md").read(),
 
 setup(
     name='mapzen.whosonfirst.search',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.search'],
-    version='0.12',
+    version=version,
     description='Simple Python wrapper for Who\'s On First search functionality',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-search',
     install_requires=[
         'geojson',
         'elasticsearch',
-        'slack.api>=0.4'	# https://github.com/whosonfirst/py-slack-api
+        'slack.api>=0.4',	# https://github.com/whosonfirst/py-slack-api
+        'mapzen.whosonfirst.utils>=0.17',
+        'mapzen.whosonfirst.placetypes>=0.11'
         ],
     dependency_links=[
         'https://github.com/whosonfirst/py-slack-api/tarball/master#egg=slack.api-0.4',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.17',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master#egg=mapzen.whosonfirst.placetypes-0.11'
         ],
     packages=packages,
     scripts=[
@@ -29,5 +34,5 @@ setup(
         'scripts/wof-es-prepare',
         'scripts/wof-es-rawquery',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-search/releases/tag/v0.12',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-search/releases/tag/' + version,
     license='BSD')
